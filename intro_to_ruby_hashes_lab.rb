fac268cf8be54b3c79fb29d99d2e0f8a571b30a8
Robert Defendini <robdefendini@gmail.com>
2020-05-18 21:46:13 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -12,12 +12,7 @@ end
 
 def monopoly_with_second_tier
 base_hash = {
-  :railroads => {},
-  :rent_in_dollars => {},
-  :names => {},
-  :pieces => 4
-  
-}
+  :railroads => {}, :pieces => 4  ,  :rent_in_dollars => {},  :names => {}}
 
 end
 
